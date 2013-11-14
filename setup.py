87934b5fa111cab6a7a398dcce2d3ffd7ea3b70a
Simon Ratcliffe <sratcliffe@ska.ac.za>
2013-11-14 12:34:24 +0200

    Tag for 0.5.1 release - see change notes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup, Extension
 import os, glob
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 
 def indir(dir, files): return [dir+f for f in files]
 def globdir(dir, files):
